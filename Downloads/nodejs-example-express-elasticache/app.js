2b49c5d544a831ac8cb7c1ede4fcfae54034e7da
rameshchundru <rramesh8422@gmail.com>
2024-04-22 23:08:08 +0530

    first version

---
+++ Downloads/nodejs-example-express-elasticache/app.js
@@ -0,0 +1,74 @@
+/**
+ * Module dependencies.
+ */
+
+const express = require('express'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  cookieParser = require('cookie-parser'),
+  fs = require('fs'),
+  filename = '/var/nodelist',
+  app = express();
+
+let MemcachedStore = require('connect-memcached')(session);
+
+function setup(cacheNodes) {
+  app.use(bodyParser.raw());
+  app.use(methodOverride());
+  if (cacheNodes.length > 0) {
+    app.use(cookieParser());
+
+    console.log('Using memcached store nodes:');
+    console.log(cacheNodes);
+
+    app.use(session({
+      secret: 'your secret here',
+      resave: false,
+      saveUninitialized: false,
+      store: new MemcachedStore({ 'hosts': cacheNodes })
+    }));
+  } else {
+    console.log('Not using memcached store.');
+    app.use(session({
+      resave: false,
+      saveUninitialized: false, secret: 'your secret here'
+    }));
+  }
+
+  app.get('/', function (req, resp) {
+    if (req.session.views) {
+      req.session.views++
+      resp.setHeader('Content-Type', 'text/html')
+      resp.send(`You are session: ${req.session.id}. Views: ${req.session.views}`)
+    } else {
+      req.session.views = 1
+      resp.send(`You are session: ${req.session.id}. No views yet, refresh the page!`)
+    }
+  });
+
+  if (!module.parent) {
+    console.log('Running express without cluster. Listening on port %d', process.env.PORT || 5000)
+    app.listen(process.env.PORT || 5000)
+  }
+}
+
+console.log("Reading elastic cache configuration")
+// Load elasticache configuration.
+fs.readFile(filename, 'UTF8', function (err, data) {
+  if (err) throw err;
+
+  let cacheNodes = []
+  if (data) {
+    let lines = data.split('\n');
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].length > 0) {
+        cacheNodes.push(lines[i])
+      }
+    }
+  }
+
+  setup(cacheNodes)
+});
+
+module.exports = app;
